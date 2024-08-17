9c58e1dabf54eb64f197e053d7833f88e6fd9292
rfdevuser <rakhisfashions@rakhisfashions.com>
2024-08-17 16:12:13 +0530

    Add files via upload

--- src/components/Assests/ShimmerShoppingCard.js
+++ src/components/Assests/ShimmerShoppingCard.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
 const ShoppingCardShimmer = ({ count }) => {
+  // Generate an array with the desired number of shimmer cards
   const shimmerCards = Array.from({ length: count }, (_, index) => (
-    <div key={index} className="w-60 h-80 bg-gradient-to-r from-pink-100 to-sky-100 p-3 flex flex-col gap-1 rounded-br-3xl animate-pulse mx-auto">
-      <div className="h-48 bg-gray-400"></div>
-      <div className="flex flex-col gap-4">
+    <div key={index} className="w-50 h-60 bg-gradient-to-r from-pink-100 to-sky-100 p-3 flex flex-col gap-1 rounded-br-3xl animate-pulse mx-auto mx-2">
+      <div className="h-48 bg-gray-400 rounded"></div>
+      <div className="flex flex-col gap-4 mt-2">
         <div className="flex flex-row justify-between">
-          <div className="flex flex-col">
-            <div className="h-6 w-40 bg-gray-500 rounded"></div>
-            <div className="h-4 w-20 bg-gray-400 rounded"></div>
-          </div>
-          <div className="h-6 w-16 bg-gray-500 rounded"></div>
+         
+          
         </div>
         <div className="h-8 w-32 bg-gray-500 rounded"></div>
       </div>
@@ -18,7 +16,7 @@ const ShoppingCardShimmer = ({ count }) => {
   ));
 
   return (
-    <div className='mx-auto mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+    <div className="mx-auto mt-10 grid grid-cols-2 gap-4 p-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4">
       {shimmerCards}
     </div>
   );
--- src/components/Assests/SimmerFabricCard.js
+++ src/components/Assests/SimmerFabricCard.js
@@ -2,21 +2,20 @@ import React from 'react';
 
 const FabricCardShimmer = ({ count }) => {
   const shimmerCards = Array.from({ length: count }, (_, index) => (
-    <div key={index} className="w-60 h-80 bg-gradient-to-r from-pink-100 to-sky-100 p-3 flex flex-col gap-1 rounded-br-3xl animate-pulse mx-auto">
-      <div className="h-48 bg-gray-400"></div>
-      <div className="flex flex-col gap-4">
+    <div key={index} className="w-50 h-60 bg-gradient-to-r from-pink-100 to-sky-100 p-3 flex flex-col gap-1 rounded-br-3xl animate-pulse mx-auto mx-2">
+      <div className="h-48 bg-gray-400 rounded"></div>
+      <div className="flex flex-col gap-4 mt-2">
         <div className="flex flex-row justify-between">
-          <div className="flex flex-col">
-            <div className="h-6 w-40 bg-gray-500 "></div>
-            <div className="h-4 w-20 bg-gray-400 "></div>
-          </div>
+         
+          
         </div>
+        <div className="h-8 w-32 bg-gray-500 rounded"></div>
       </div>
     </div>
   ));
 
   return (
-    <div className='mx-auto mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+    <div className="mx-auto mt-10 grid grid-cols-2 gap-4 p-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4">
       {shimmerCards}
     </div>
   );
